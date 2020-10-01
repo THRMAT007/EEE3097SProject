f2a4d44ace51adb0d388a75a69ae694d5005f880
Matthew <THRAMT007@myuct.ac.za>
2020-10-01 10:09:11 +0200

    started on the Water sensor class

---
+++ Waterlevel.py
@@ -0,0 +1,15 @@
+#Crated by Matthew Thorburn
+#EEE3097S porject
+#class to handle the Water level measurments for the API
+
+
+def initWater():
+	wlevel=0;
+	wMax=100
+
+
+def getPercentFull():
+	retun (wlevel/wMax)
+
+def setMax(m):
+	wMax=m;
