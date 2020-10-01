4981d86d73bcbdb261ad0626231acdba47cd78b2
Matthew <THRAMT007@myuct.ac.za>
2020-10-01 10:37:03 +0200

    finnished basic layout

--- Waterlevel.py
+++ Waterlevel.py
@@ -2,14 +2,17 @@
 #EEE3097S porject
 #class to handle the Water level measurments for the API
 
-
 def initWater():
 	wlevel=0;
 	wMax=100
 
 
 def getPercentFull():
-	retun (wlevel/wMax)
+	retun (100*wlevel/wMax)
 
 def setMax(m):
 	wMax=m;
+
+def getWaterLevel()
+	wlevel = #get sensor data
+	return wlevel
