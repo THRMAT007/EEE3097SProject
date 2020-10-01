50fa97a94c96b7761e6ad7165cf7a3142a0ddb74
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2020-10-01 14:04:10 +0200

    Add files via upload

---
+++ Temperature.py
@@ -0,0 +1,35 @@
+#Made by Ross Lakey, the glorious leader of this project, not that E-boy matthew
+#EEE3097S *PROJECT not PORJECT MATTHEW*
+#░░░░█─────────────█──▀──
+
+#░░░░▓█───────▄▄▀▀█──────
+
+#░░░░▒░█────▄█▒░░▄░█─────
+
+#░░░░░░░▀▄─▄▀▒▀▀▀▄▄▀──DO─
+
+#░░░░░░░░░█▒░░░░▄▀────IT──
+
+#▒▒▒░░░░▄▀▒░░░░▄▀───FOR──
+
+#▓▓▓▓▒░█▒░░░░░█▄───BILLY──
+
+#█████▀▒░░░░░█░▀▄────────
+
+#█████▒▒░░░▒█░░░▀▄───────
+
+#███▓▓▒▒▒▀▀▀█▄░░░░█──────
+
+#▓██▓▒▒▒▒▒▒▒▒▒█░░░░█─────
+
+#▓▓█▓▒▒▒▒▒▒▓▒▒█░░░░░█────
+
+#░▒▒▀▀▄▄▄▄█▄▄▀░░░░░░░█─
+#class to handle the temperature measurments for the API
+def GetWaterTemp():
+    wTemp = #get inner probe data
+    return wTemp
+
+def GetOutSideTemp():
+    oTemp = #get outer probe data
+    return oTemp
